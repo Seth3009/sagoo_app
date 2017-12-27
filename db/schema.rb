b12880dbe6f71cda8c7dc27f6a76150ca4f6e833
Setiadi <Setiadi.seth@gmail.com>
2017-12-27 03:19:49 +0000

    Create location Model and else

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171226030603) do
+ActiveRecord::Schema.define(version: 20171226035609) do
 
   create_table "employees", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20171226030603) do
     t.string   "name"
     t.decimal  "salary"
     t.text     "note"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
