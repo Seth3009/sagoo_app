4da91775201c9c24da7469c6b5b16fab5b91ada5
Setiadi <Setiadi.seth@gmail.com>
2018-02-19 12:23:33 +0000

    Revisi di model potongan employee

--- app/models/potongan_employee.rb
+++ app/models/potongan_employee.rb
@@ -9,9 +9,11 @@ class PotonganEmployee < ActiveRecord::Base
   
   def self.update_amount_kasbon(emp,bulan,tahun)
     @ks = Kasbon.joins('left join detail_kasbons on detail_kasbons.kasbon_id = kasbons.id').where('kasbons.employee_id = ? and detail_kasbons.tanggal_bayar = ?', emp, Date.parse(tahun.to_s+ "-" + bulan.to_s + "-1")).select('kasbons.id','detail_kasbons.jumlah_bayar as bayar').first
-    @pot = PotonganEmployee.where('employee_id = ? and potongan_employees.kasbon_id =?',emp, @ks.id)
-    @pot.each do |pot|
-      PotonganEmployee.update(pot.id, amount: @ks.bayar)
+    if @ks
+      @pot = PotonganEmployee.where('employee_id = ? and potongan_employees.kasbon_id =?',emp, @ks.id)
+      @pot.each do |pot|
+        PotonganEmployee.update(pot.id, amount: @ks.bayar)
+      end
     end
   end
 end
