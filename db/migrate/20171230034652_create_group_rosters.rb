292e4677a9ad3667d14dca0e63858d68a6fc8d4c
Setiadi <Setiadi.seth@gmail.com>
2018-05-05 03:12:00 +0000

    Create_Group_roster

--- db/migrate/20171230034652_create_group_rosters.rb
+++ db/migrate/20171230034652_create_group_rosters.rb
@@ -1,7 +1,7 @@
 class CreateGroupRosters < ActiveRecord::Migration
   def change
     create_table :group_rosters do |t|
-      t.references :group, index: true, foreign_key: true
+      # t.references :group, index: true, foreign_key: true
       t.references :roster, index: true, foreign_key: true
       t.decimal :amount
 
